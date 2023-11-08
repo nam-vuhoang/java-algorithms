6bfa557593b03ea3611ea6b3cf8bc15cca5af1c3
nam <nam@nams-mbp.home>
2023-11-08 16:11:44 +0200

    Cleanup code.

--- src/test/java/org/namvu/utils/BinarySearchAlgorithmTests.java
+++ src/test/java/org/namvu/utils/BinarySearchAlgorithmTests.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.namvu.utils.BinarySearchAlgorithm;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
